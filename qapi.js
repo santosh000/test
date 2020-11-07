c2a673114a5ce920486466b3c83e6d8ce1abf01a
Santosh <ucancallmesan@gmail.com>
2020-11-08 04:05:14 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa71f3779530b001fd1fb58",
+				"5fa71f3779530b001fd1fb5a"
+			]
+		},
+		"5fa71f3779530b001fd1fb58": {
+			"id": "5fa71f3779530b001fd1fb58",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa71f3779530b001fd1fb5a": {
+			"id": "5fa71f3779530b001fd1fb5a",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,204 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Image, Input, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { StackItem, Stack, Section, Override, Formspree, SocialMedia, Menu } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			padding="140px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://images.unsplash.com/photo-1486074051793-e41332bf18fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80) 50% 15%/cover"
+			color="--light"
+			font="--base"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="50px"
+					text-transform="uppercase"
+					font="100 132px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					Mary
+					<br />
+					Cornell
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				>
+					<Text margin="0px">
+						About me
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						My name is Mary Cornell, born and raised in France.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						I've been working as a professional photographer and videographer for more than 12 years.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						CDS Documentary Essay Prize and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
+			<Text as="h1" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
+			</Text>
+			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1501870190084-cdf29f15ef87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1503341960582-b45751874cf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1503342394128-c104d54dba01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
+				</Box>
+			</Box>
+		</Section>
+		<Section background="--color-light" color="--dark" padding="64px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+						margin="0"
+					>
+						Contact
+					</Text>
+					<Text font="--headline2" max-width="500px" margin="10px 0 0 0">
+						Email us, we would love to hear form you
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Formspree>
+						<Stack gap="16px">
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Name
+								</Text>
+								<Input max-width="400px" width="100%" name="name" />
+							</StackItem>
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Email
+								</Text>
+								<Input max-width="400px" width="100%" type="email" name="email" />
+							</StackItem>
+							<StackItem width="100%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Message
+								</Text>
+								<Input as="textarea" rows="4" width="100%" name="message" />
+							</StackItem>
+							<StackItem width="100%">
+								<Button>
+									Send
+								</Button>
+							</StackItem>
+						</Stack>
+					</Formspree>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-dark" padding="60px 0" sm-padding="40px 0">
+			<SocialMedia
+				facebook="https://www.facebook.com/quarklyapp/"
+				twitter="https://twitter.com/quarklyapp"
+				youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+				vkontakte="https://vk.com/quarklyapp"
+				telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ"
+			>
+				<Override
+					slot="link"
+					border-radius="50%"
+					color="--grey"
+					hover-color="--light"
+					background="transparent"
+					hover-background="transparent"
+					margin="0 8px"
+				/>
+			</SocialMedia>
+		</Section>
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				hello@company.com
+			</Link>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
